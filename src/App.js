59eb856c5e7dd8b5782970e3244763240a8ff8f1
skks1991 <basher.send@gmail.com>
2024-09-28 01:08:26 +0600

    updated

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {HashRouter, Route, Routes, Link} from 'react-router-dom';
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
+
+function App() {
+  return (
+    <HashRouter>
+    <div className="App">
+      <h1>React JS CRUD Operations</h1>
+        <Routes>
+          <Route path ='/' element = {<EmpListing/>}></Route>
+          <Route path ='/employee/create' element = {<EmpCreate/>}></Route>
+          <Route path ='/employee/detail/:empid' element = {<EmpDetail/>}></Route>
+          <Route path ='/employee/edit/:empid' element = {<EmpEdit/>}></Route>
+        </Routes>
+      
+    </div>
+    </HashRouter>
+  );
+}
+
+export default App;
